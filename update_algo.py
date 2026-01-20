7f33466e6ff212b74693c6331e31f4f9154c9ea2
nanansuh234 <140320514+nanansuh234@users.noreply.github.com>
2026-01-21 05:54:55 +0700

    Update update_algo.py

--- update_algo.py
+++ update_algo.py
@@ -3,7 +3,6 @@ import json
 import os
 
 API_KEY = os.getenv('YT_API_KEY')
-# List Channel ID yang mau lo "paksain" jadi algoritma bareng
 CHANNELS_NAMES = [
     "Warta Kota Production", "Liputan6", "Just Pranks", "Firstpost", 
     "Muzen - Musik Netizen", "Planet News", "Peaceful Rest", "Muse Asia",
@@ -23,7 +22,7 @@ CHANNELS_NAMES = [
     "MASAFUMI KAWAMOTO", "The Dodo", "ERA MAHDI", "Ghibran Arrazi", "slob",
     "KAUM HADI", "Anons Super", "Ricky Suwarno", "VoxAlchemy", "Ngaji Roso (Mas Hendra)",
     "KONTAN TV", "Watchdoc Unity", "Muhammad Hamdan AlGhozali", "Mr. Ledger",
-    "Bale Films", "Liputan 6 SCTV Daerah", "𝓢𝓪𝓽𝓻𝓲𝓪 𝓝𝓾𝓼𝓪𝓷𝓽𝓪𝓻𝓪💫", "YUSHA CHANNEL",
+    "Bale Films", "Liputan 6 SCTV Daerah", "𝓢𝓪𝓽𝓻𝓲𝓪 𝓝𝓾𝓼𝓪𝓷𝓽𝓪𝓻าะ", "YUSHA CHANNEL",
     "MerdekaDotCom", "INFO AWIBISANA", "Esha P", "MICHAEKAL", "Pena Cakra",
     "Fittest Flat Earther", "Permata Islam", "Caveine", "Anak Bapak Aing",
     "Prof Jiang Uncut", "Bossman Mardigu", "Old World Ledger", "FINMUSIC",
@@ -31,26 +30,32 @@ CHANNELS_NAMES = [
     "Channel Anak TUHAN", "Matt Peci Channel"
 ]
 
-# Tambahkan ID channel lo sendiri sebagai prioritas utama
-MY_CHANNEL_ID = 'UC1sz4pommnQQY5xKZ9VadxQ'
+def get_channel_id(name):
+    url = f"https://www.googleapis.com/youtube/v3/search?key={API_KEY}&q={name}&type=channel&part=id&maxResults=1"
+    res = requests.get(url).json()
+    if 'items' in res and len(res['items']) > 0:
+        return res['items'][0]['id']['channelId']
+    return None
 
-def get_videos():
+def get_latest_videos():
     all_videos = []
-    for channel_id in CHANNELS:
-        url = f"https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={channel_id}&part=snippet,id&order=date&maxResults=5"
-        res = requests.get(url).json()
-        if 'items' in res:
-            for item in res['items']:
-                all_videos.append({
-                    'title': item['snippet']['title'],
-                    'videoId': item['id']['videoId'],
-                    'thumbnail': item['snippet']['thumbnails']['high']['url'],
-                    'channel': item['snippet']['channelTitle']
-                })
+    # Kita ambil 15 channel pertama dulu agar kuota API aman
+    for name in CHANNELS_NAMES[:15]: 
+        channel_id = get_channel_id(name)
+        if channel_id:
+            url = f"https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={channel_id}&part=snippet,id&order=date&type=video&maxResults=2"
+            res = requests.get(url).json()
+            if 'items' in res:
+                for item in res['items']:
+                    all_videos.append({
+                        'title': item['snippet']['title'],
+                        'videoId': item['id']['videoId'],
+                        'thumbnail': item['snippet']['thumbnails']['high']['url'],
+                        'channel': item['snippet']['channelTitle']
+                    })
     
     with open('data.json', 'w') as f:
         json.dump(all_videos, f, indent=4)
 
 if __name__ == "__main__":
-
-    get_videos()
+    get_latest_videos()
