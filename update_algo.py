3ed0238f5e3c0173e5e8feb1cc2ad21f944038d8
nanansuh234 <140320514+nanansuh234@users.noreply.github.com>
2026-01-21 05:52:53 +0700

    Update update_algo.py

--- update_algo.py
+++ update_algo.py
@@ -4,7 +4,35 @@ import os
 
 API_KEY = os.getenv('YT_API_KEY')
 # List Channel ID yang mau lo "paksain" jadi algoritma bareng
-CHANNELS = ['UC...', 'UC...'] 
+CHANNELS_NAMES = [
+    "Warta Kota Production", "Liputan6", "Just Pranks", "Firstpost", 
+    "Muzen - Musik Netizen", "Planet News", "Peaceful Rest", "Muse Asia",
+    "Times Of India", "Status Coup News", "4K Relaxation Channel", "YHS Media",
+    "Music Brokers", "Animortis", "Animus Morphosis", "Heisei Sound System",
+    "The Pedia", "Johnny 5 Music", "Kaitoo74", "Artificial Harmony",
+    "Af Music cover", "VinylByte", "Astral Atom", "Nanda Pradika",
+    "Baku Hantam", "Askjapan", "JV Music Collaboration", "Grim Fuel",
+    "FAITH IN A FLASH", "Tanah Jawa Dwipa", "Kabar Langit", "MIKIRO",
+    "Pandji Pragiwaksono", "Pandangan Mata", "Cozy Day ASMR 癒し", "BENCANA POPULER",
+    "Grey District Audio", "MIND LIFT", "MATA KETIGA", "Ghibli Silent Days",
+    "ANTARA News", "tribunjabar video", "Business Inspiration", "Benn Jordan",
+    "Ferry Irwandi", "Kompascom Reporter on Location", "yomikez", "Aura Haruno",
+    "GESARA news", "GEMBULIKUM", "Sekilas Uang", "TOP PINK", "Kompas.com",
+    "Aini Musik", "The Royal Nation", "Bennix", "guru gembul", "Anonymous Prophet",
+    "Dokumentari", "Dongeng uNick LaLa (DNL)", "Fajrul Fx", "Yann Animasi",
+    "MASAFUMI KAWAMOTO", "The Dodo", "ERA MAHDI", "Ghibran Arrazi", "slob",
+    "KAUM HADI", "Anons Super", "Ricky Suwarno", "VoxAlchemy", "Ngaji Roso (Mas Hendra)",
+    "KONTAN TV", "Watchdoc Unity", "Muhammad Hamdan AlGhozali", "Mr. Ledger",
+    "Bale Films", "Liputan 6 SCTV Daerah", "𝓢𝓪𝓽𝓻𝓲𝓪 𝓝𝓾𝓼𝓪𝓷𝓽𝓪𝓻𝓪💫", "YUSHA CHANNEL",
+    "MerdekaDotCom", "INFO AWIBISANA", "Esha P", "MICHAEKAL", "Pena Cakra",
+    "Fittest Flat Earther", "Permata Islam", "Caveine", "Anak Bapak Aing",
+    "Prof Jiang Uncut", "Bossman Mardigu", "Old World Ledger", "FINMUSIC",
+    "Tempodotco", "Inti channel", "GAGAL GAJIAN", "TUKANG AB", "FASKHO OFFICIAL",
+    "Channel Anak TUHAN", "Matt Peci Channel"
+]
+
+# Tambahkan ID channel lo sendiri sebagai prioritas utama
+MY_CHANNEL_ID = 'UC1sz4pommnQQY5xKZ9VadxQ'
 
 def get_videos():
     all_videos = []
@@ -24,4 +52,5 @@ def get_videos():
         json.dump(all_videos, f, indent=4)
 
 if __name__ == "__main__":
+
     get_videos()
